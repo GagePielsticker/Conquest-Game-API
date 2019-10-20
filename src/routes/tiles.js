ad40dfa29d2e8a85a3b66b4c99d8703589e76390
Gage Pielsticker <gagepays@gmail.com>
2019-10-20 15:01:47 -0500

    Changed use to get

--- src/routes/tiles.js
+++ src/routes/tiles.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 module.exports = client => {
-  router.use('/tiles/:xPos/:yPos', (req, res) => {
+  router.get('/tiles/:xPos/:yPos', (req, res) => {
     client.game.getTile(Number(req.params.xPos), Number(req.params.yPos))
       .then(tile => res.json(tile))
       .catch(e => res.json({ error: e }))
