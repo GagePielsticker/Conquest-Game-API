a785320e3b97c81275cecd77fadc5973dd560bc9
JPBBerry <jb@jt3ch.net>
2019-10-22 16:58:26 -0400

    Hello event

--- src/websocket/WebsocketManager.js
+++ src/websocket/WebsocketManager.js
@@ -27,6 +27,7 @@ class WebsocketManager extends EventEmitter {
     const wsID = new Date().getTime()
     console.log(`${new Date()} new websocket connection, ID: ${wsID}`)
     connection.id = wsID
+    connection.hello = false
     this.clients.set(wsID, connection)
     connection.on('close', (code, reason) => {
       console.log(`${new Date()} websocket connection closed, ID: ${wsID}; Code: ${code}, Reason: ${reason}`)
@@ -40,8 +41,30 @@ class WebsocketManager extends EventEmitter {
       this.emit('raw', data)
       this.emit(data.event, data.data)
 
+      if (data.event === 'hello') {
+        const newConnect = this.clients.get(wsID)
+        newConnect.hello = true
+        this.clients.set(wsID, newConnect)
+      }
       if (data.event === 'heartbeat') this.ack(wsID)
     })
+    this.handleHello(connection)
+  }
+
+  handleHello (connection) {
+    this.send(connection.id, 'hello', {
+      id: connection.id,
+      hb: this.client.settings.ws.heartbeat_interval
+    })
+    setTimeout(() => {
+      const con = this.clients.get(connection.id)
+      if (!con || !con.hello) {
+        console.log(`${new Date()} Never received hello on ${connection.id}. Closed.`)
+        connection.close()
+      } else {
+        console.log(`${new Date()} received hello event on ${connection.id}. Connecting finished.`)
+      }
+    }, 5000)
   }
 
   send (wsID, event, data) {
